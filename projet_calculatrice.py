f5995b2af3a76dc2bd61d7f8546532ec0b20dbe7
uvsq22011110 <uvsq22011110@users.noreply.github.com>
2020-12-17 10:17:14 +0100

    fontion

--- projet_calculatrice.py
+++ projet_calculatrice.py
@@ -2,3 +2,29 @@
 import tkinter as tk 
 def b_1
     pass
+def b_2
+    pass
+def b_3
+    pass
+def b_4
+    pass
+def b_5
+    pass
+def b_6
+    pass
+def b_7
+    pass
+def b_8
+    pass
+def b_9
+    pass
+def b_0
+    pass
+def b_plus
+    pass
+def b_moins
+    pass
+def b_fois
+    pass
+def b_div
+    pass
